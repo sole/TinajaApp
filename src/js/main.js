b11bdbde026d6a65458c9f22f5b2f1755e101159
sole <sole@mozilla.com>
2014-09-20 22:36:09 +0100

    set up basic graph + also start gum with fake = true

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,39 @@
+require('./gumPolyfill.js');
+
+var audioContext;
+var audioAnalyser;
+var audioGain;
+var audioSource;
 var tinaja = document.getElementById('tinaja');
 
-setInterval(function() {
+/*setInterval(function() {
 	var scale = 1 + 3 * Math.random();
 	scaleTinaja(scale);
-}, 100);
+}, 100);*/
 
+init();
+startAudioInput();
+
+function init() {
+	audioContext = new AudioContext();
+	audioAnalyser = audioContext.createAnalyser();
+	audioAnalyser.connect(audioContext.destination);
+	audioGain = audioContext.createGain();
+	audioGain.connect(audioAnalyser);
+}
+
+function startAudioInput() {
+	navigator.getUserMedia(
+		{ audio: true, fake: true },
+		function yay(stream) {
+			audioSource = audioContext.createMediaStreamSource(stream);
+			audioSource.connect(audioGain);
+		},
+		function nope(err) {
+			console.err("oh noes", err);
+		}
+	);
+}
 
 function scaleTinaja(value) {
 	var scale = (value * 1.0).toFixed(2);
