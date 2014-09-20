c9656007d86fbf29dfec76ebc0e6b5bd9988ec95
sole <sole@mozilla.com>
2014-09-20 22:55:39 +0100

    scale that tinaja!

--- src/js/main.js
+++ src/js/main.js
@@ -2,14 +2,10 @@ require('./gumPolyfill.js');
 
 var audioContext;
 var audioAnalyser;
+var audioAnalyserData;
 var audioGain;
 var audioSource;
 var tinaja = document.getElementById('tinaja');
-
-/*setInterval(function() {
-	var scale = 1 + 3 * Math.random();
-	scaleTinaja(scale);
-}, 100);*/
 
 init();
 startAudioInput();
@@ -17,20 +13,27 @@ startAudioInput();
 function init() {
 	audioContext = new AudioContext();
 	audioAnalyser = audioContext.createAnalyser();
-	audioAnalyser.connect(audioContext.destination);
+	audioAnalyser.fftSize = 2048;
+	audioAnalyser.smoothingTimeConstant = 0.8;
+	audioAnalyserData = new Uint8Array(audioAnalyser.frequencyBinCount);
+	//audioAnalyser.connect(audioContext.destination);
 	audioGain = audioContext.createGain();
 	audioGain.connect(audioAnalyser);
 }
 
 function startAudioInput() {
 	navigator.getUserMedia(
-		{ audio: true, fake: true },
+		{
+			audio: true,
+			fake: true
+		},
 		function yay(stream) {
 			audioSource = audioContext.createMediaStreamSource(stream);
 			audioSource.connect(audioGain);
+			animate();
 		},
 		function nope(err) {
-			console.err("oh noes", err);
+			console.err('oh noes', err);
 		}
 	);
 }
@@ -39,4 +42,15 @@ function scaleTinaja(value) {
 	var scale = (value * 1.0).toFixed(2);
 	var invPerc = ((100.0 / scale).toFixed(2)) + '%';
 	tinaja.style.transform = 'translate(100%, 100%) scale(' + scale + ')';
+}
+
+function animate(t) {
+
+	requestAnimationFrame(animate);
+
+	audioAnalyser.getByteFrequencyData(audioAnalyserData);
+
+	var v = audioAnalyserData[0];
+	scaleTinaja( 1 + 2 * v/255.0 );
+
 }
